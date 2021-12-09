93d9a0886e34df589870eae44aaeaacde4825f2e
2019alalguiz <zyad.alalgui@student-cs.com>
2021-12-09 14:05:55 +0100

    modify input read template file

--- src/inputReadTemplate.java
+++ src/inputReadTemplate.java
@@ -1,32 +1,38 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class inputReadTemplate {
 
     public static void main(String[] args) throws IOException {
-        int testInputAnswer = puzzleAnswer("./src/day/testInput.txt");
-        int inputAnswer = puzzleAnswer("./src/day/input.txt");
+        ArrayList<Integer> testInputAnswer = puzzleAnswer("./src/day6/testInput.txt");
+        ArrayList<Integer> inputAnswer = puzzleAnswer("./src/day6/input.txt");
 
-        System.out.println(testInputAnswer);
-        System.out.println(inputAnswer);
+        System.out.println(testInputAnswer.size());
+        System.out.println(inputAnswer.size());
     }
 
-    private static Integer puzzleAnswer(String input) throws IOException {
+    private static ArrayList<Integer> readInput(String input) throws IOException {
         FileReader fileReader = new FileReader(input);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
         String currentLine = bufferedReader.readLine();
 
-        while(true) {
-            currentLine = bufferedReader.readLine();
-            if(currentLine == null) {
-                break;
-            }
-        };
-
+        ArrayList<Integer> readInput = Arrays.stream(currentLine.split(","))
+                .map(fish -> Integer.parseInt(fish))
+                .collect(Collectors.toCollection(ArrayList::new));
         bufferedReader.close();
-        return(null);
+        return(readInput);
+    }
+
+    private static ArrayList<Integer> puzzleAnswer(String input) throws IOException {
+
+        ArrayList<Integer> answer = readInput(input);
+
+        return answer;
     }
 
 }
